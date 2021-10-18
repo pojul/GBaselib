7254f20a4649bd7631b88c571f3a2035ee8c94bc
pojul <2443974192@qq.com>
2021-10-18 22:12:03 +0800

    add sputil

--- src/main/java/com/base/gbase_lib/utils/SpUtil.java
+++ src/main/java/com/base/gbase_lib/utils/SpUtil.java
@@ -1,4 +1,269 @@
 package com.base.gbase_lib.utils;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import com.google.gson.Gson;
+import java.util.Map;
+
 public class SpUtil {
+
+    private SharedPreferences sp;
+    private SharedPreferences.Editor editor;
+    private volatile static SpUtil instance;
+    public Context applicationContext;
+
+    public static SpUtil getInstance(){
+        if(instance == null){
+            synchronized(SpUtil.class){
+                if(instance == null)
+                    instance = new SpUtil();
+            }
+        }
+        return instance;
+    }
+
+    /**
+     * SPUtils构造函数
+     * <p>在Application中初始化</p>
+     *
+     * @param context Application上下文
+     * @param spName  spName
+     */
+    public void init(Context context, String spName) {
+        sp = context.getSharedPreferences(spName, Context.MODE_PRIVATE);
+        editor = sp.edit();
+        editor.apply();
+        this.applicationContext = context;
+    }
+
+    /**
+     * SP中写入String类型value
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public void putString(String key, String value) {
+        editor.putString(key, value).apply();
+    }
+
+    /**
+     * SP中读取String
+     *
+     * @param key 键
+     * @return 存在返回对应值，不存在返回默认值{@code null}
+     */
+    public String getString(String key) {
+        return getString(key, null);
+    }
+
+    /**
+     * SP中读取String
+     *
+     * @param key          键
+     * @param defaultValue 默认值
+     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
+     */
+    public String getString(String key, String defaultValue) {
+        return sp.getString(key, defaultValue);
+    }
+
+    /**
+     * SP中写入int类型value
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public void putInt(String key, int value) {
+        editor.putInt(key, value).apply();
+    }
+
+    /**
+     * SP中读取int
+     *
+     * @param key 键
+     * @return 存在返回对应值，不存在返回默认值-1
+     */
+    public int getInt(String key) {
+        return getInt(key, -1);
+    }
+
+    /**
+     * SP中读取int
+     *
+     * @param key          键
+     * @param defaultValue 默认值
+     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
+     */
+    public int getInt(String key, int defaultValue) {
+        return sp.getInt(key, defaultValue);
+    }
+
+    /**
+     * SP中写入long类型value
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public void putLong(String key, long value) {
+        editor.putLong(key, value).apply();
+    }
+
+    /**
+     * SP中读取long
+     *
+     * @param key 键
+     * @return 存在返回对应值，不存在返回默认值-1
+     */
+    public long getLong(String key) {
+        return getLong(key, -1L);
+    }
+
+    /**
+     * SP中读取long
+     *
+     * @param key          键
+     * @param defaultValue 默认值
+     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
+     */
+    public long getLong(String key, long defaultValue) {
+        return sp.getLong(key, defaultValue);
+    }
+
+    /**
+     * SP中写入float类型value
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public void putFloat(String key, float value) {
+        editor.putFloat(key, value).apply();
+    }
+
+    /**
+     * SP中读取float
+     *
+     * @param key 键
+     * @return 存在返回对应值，不存在返回默认值-1
+     */
+    public float getFloat(String key) {
+        return getFloat(key, -1f);
+    }
+
+    /**
+     * SP中读取float
+     *
+     * @param key          键
+     * @param defaultValue 默认值
+     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
+     */
+    public float getFloat(String key, float defaultValue) {
+        return sp.getFloat(key, defaultValue);
+    }
+
+    /**
+     * SP中写入boolean类型value
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public void putBoolean(String key, boolean value) {
+        editor.putBoolean(key, value).apply();
+    }
+
+    /**
+     * SP中读取boolean
+     *
+     * @param key 键
+     * @return 存在返回对应值，不存在返回默认值{@code false}
+     */
+    public boolean getBoolean(String key) {
+        return getBoolean(key, false);
+    }
+
+    /**
+     * SP中读取boolean
+     *
+     * @param key          键
+     * @param defaultValue 默认值
+     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
+     */
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return sp.getBoolean(key, defaultValue);
+    }
+
+    /**
+     * SP中获取所有键值对
+     *
+     * @return Map对象
+     */
+    public Map<String, ?> getAll() {
+        return sp.getAll();
+    }
+
+    /**
+     * SP中移除该key
+     *
+     * @param key 键
+     */
+    public void remove(String key) {
+        editor.remove(key).apply();
+    }
+
+    /**
+     * SP中是否存在该key
+     *
+     * @param key 键
+     * @return {@code true}: 存在<br>{@code false}: 不存在
+     */
+    public boolean contains(String key) {
+        return sp.contains(key);
+    }
+
+    /**
+     * SP中清除所有数据
+     */
+    public void clear() {
+        editor.clear().apply();
+    }
+
+    /**
+     * @param key
+     * @param objClass
+     */
+    public Object getObj(String key, Class objClass) {
+        if (null == applicationContext) {
+            return null;
+        }
+        try{
+            String json = getString(key);
+            if(json==null || json.isEmpty()){
+                return null;
+            }
+            return new Gson().fromJson(json, objClass);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * @param key
+     * @param obj
+     */
+    public boolean setObj(String key, Object obj) {
+        if (null == applicationContext || obj == null) {
+            return false;
+        }
+        try{
+            String newJson = new Gson().toJson(obj);
+            if(newJson==null || newJson.isEmpty()){
+                return false;
+            }
+            putString(key, newJson);
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
